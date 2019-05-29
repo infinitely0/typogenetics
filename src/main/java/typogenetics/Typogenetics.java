1a2ed9f411480fc405473b3425211d1835f68ddb
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2019-05-29 14:40:27 +0100

    Init

---
+++ src/main/java/typogenetics/Typogenetics.java
@@ -0,0 +1,9 @@
+package typogenetics;
+
+public class Typogenetics {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/typogenetics/TypogeneticsTest.java
@@ -0,0 +1,14 @@
+package typogenetics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TypogeneticsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
